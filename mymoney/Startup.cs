f5d649cfd939a58819fe403304d118a804fd996f
Thomas, Emmanuel Sebastien <ethomas32@gatech.edu>
2018-01-01 16:24:15 -0800

    Add project files.

---
+++ mymoney/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(mymoney.Startup))]
+namespace mymoney
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
